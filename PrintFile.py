374fa2e23a6ffcaab3ef26123ee484c1c6ee2660
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-03-11 17:53:13 +1100

    Create PrintFile.py

---
+++ PrintFile.py
@@ -0,0 +1,8 @@
+workflow_name = "WorkflowScripts::PrintFile"
+source_url = "https://github.com/uaineteine/WorkflowScripts/"
+
+print(f"{'=' * 40}")
+print(f"{' ' * 10} {workflow_name}")
+print(f"{'=' * 40}")
+print(f"Source: {source_url}")
+print(f"{'=' * 40}")
